340dfa16a52ef82ef4302bbdc647d69156cebd10
Uspehgp <uspehgp@gmail.com>
2022-01-13 15:47:17 +0300

    Creation Functions of

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -26,61 +26,12 @@ import {ajax} from 'rxjs/ajax';
 export class AboutComponent implements OnInit {
 
   ngOnInit() {
-
-    // const ajax$ = ajax('https://random-data-api.com/api/name/random_name');
-    // ajax$.subscribe(data => console.log('Subs1', data.response.first_name));
-    // ajax$.subscribe(data => console.log('Subs2', data.response.first_name));
-    // ajax$.subscribe(data => console.log('Subs3', data.response.first_name));
-    // const observable$ = new Observable<number>(subscriber => {
-    //   let counter = 0;
-    //   const intervalId = setInterval(() => {
-    //     console.log('emitted ', counter);
-    //     subscriber.next(counter++);
-    //   }, 1000);
-    //   return () => {
-    //     clearInterval(intervalId);
-    //   };
-    // });
-    //
-    // const subscription = observable$.subscribe(value => console.log(value));
-    //
-    // setTimeout(() => {
-    //   subscription.unsubscribe();
-    //   console.log('Unsubscribed');
-    // }, 7000);
-
+    of('Alice', 'Ben', 'Charly').subscribe((v) => console.log(v));
   }
-//   const helloButton = document.querySelector('button#hello');
-//
-//   const helloClick$ = new Observable<MouseEvent>(subscriber => {
-//     helloButton.addEventListener('click', (event) => {
-//       subscriber.next(event);
-//     });
-//   });
-//
-//   helloClick$.subscribe(
-//     event => console.log('Sub 1:', event.type, event.x, event.y)
-// );
-//
-//   setTimeout(() => {
-//   console.log('Subscription 2 starts');
-//   helloClick$.subscribe(
-//     event => console.log('Sub 2:', event.type, event.x, event.y)
-// );
-// }, 5000);
 
   run($event: MouseEvent) {
-    const runClick = new Observable(subscriber => {
-      const clickBut = new EventEmitter((event) => {
-        subscriber.next(event);
-      });
-    });
-
-    runClick.subscribe(event => console.log(event));
-
 
   }
-
 }
 
 
