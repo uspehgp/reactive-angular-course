29112df76c4acc7901c64f44eb76d8b2022990ef
Uspehgp <uspehgp@gmail.com>
2022-01-13 16:15:57 +0300

    implementing custom  of: ourOwnOf

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -26,10 +26,31 @@ import {ajax} from 'rxjs/ajax';
 export class AboutComponent implements OnInit {
 
   ngOnInit() {
-    of('Alice', 'Ben', 'Charly').subscribe({
-      next: (v) => console.log(v),
-      complete: () => console.log('completed')
+    ourOwnOf('Alice', 'Ben', 'Charlie').subscribe({
+      next: value => console.log(value),
+      complete: () => console.log('Completed')
     });
+
+// const names$ = new Observable<string>(subscriber => {
+//   subscriber.next('Alice');
+//   subscriber.next('Ben');
+//   subscriber.next('Charlie');
+//   subscriber.complete();
+// });
+
+// names$.subscribe({
+//   next: value => console.log(value),
+//   complete: () => console.log('Completed')
+// });
+
+    function ourOwnOf(...args: string[]): Observable<string> {
+      return new Observable<string>(subscriber => {
+        for (let i = 0; i < args.length; i++) {
+          subscriber.next(args[i]);
+        }
+        subscriber.complete();
+      });
+    }
   }
 
   run($event: MouseEvent) {
