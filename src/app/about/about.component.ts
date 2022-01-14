6a4744695945614e85ff0c328e6d813a6bb424b5
Uspehgp <uspehgp@gmail.com>
2022-01-14 18:17:47 +0300

    implementing from function for promise

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -26,10 +26,19 @@ import {ajax} from 'rxjs/ajax';
 export class AboutComponent implements OnInit {
 
   ngOnInit() {
-    from(['Alice', 'Ben', 'Charlie']).subscribe({
-      next: value => console.log(value),
-      // complete: () => console.log('Completed')
+    const somePromise = new Promise((resolve, reject) => {
+      resolve('Resolved!');
+      // reject('Rejected!');
     });
+
+    const observableFromPromise$ = from(somePromise);
+
+    observableFromPromise$.subscribe({
+      next: value => console.log(value),
+      // error: err => console.log('Error:', err),
+      complete: () => console.log('Completed')
+    });
+
 
 // const names$ = new Observable<string>(subscriber => {
 //   subscriber.next('Alice');
