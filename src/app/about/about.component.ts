46851a905f8e40af107c257c74f7bddb8e987143
Uspehgp <uspehgp@gmail.com>
2022-01-15 17:55:47 +0300

    implementing forkJoin - Handle multiple HTTP calls

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,7 +12,7 @@ import {
   Subject,
   BehaviorSubject,
   AsyncSubject,
-  ReplaySubject, from
+  ReplaySubject, from, forkJoin
 } from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {ajax} from 'rxjs/ajax';
@@ -27,49 +27,19 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-    console.log('App started');
+    const randomName$ = ajax('https://random-data-api.com/api/name/random_name');
 
-    const interval$ = new Observable<number>(subscriber => {
-      let counter = 0;
+    const randomNation$ = ajax('https://random-data-api.com/api/nation/random_nation');
 
-      const intervalId = setInterval(() => {
-        console.log('Timeout!');
-        subscriber.next(counter++);
-      }, 1000);
+    const randomFood$ = ajax('https://random-data-api.com/api/food/random_food');
 
-      return () => clearInterval(intervalId);
-    });
+    // randomName$.subscribe(ajaxResponse => console.log(ajaxResponse.response.first_name));
+    // randomNation$.subscribe(ajaxResponse => console.log(ajaxResponse.response.capital));
+    // randomFood$.subscribe(ajaxResponse => console.log(ajaxResponse.response.dish));
 
-    const subscription = interval$.subscribe({
-      next: value => console.log(value),
-      complete: () => console.log('Completed')
-    });
-
-    setTimeout(() => {
-      subscription.unsubscribe();
-      console.log('Unsubscribe');
-    }, 5000);
-
-// const names$ = new Observable<string>(subscriber => {
-//   subscriber.next('Alice');
-//   subscriber.next('Ben');
-//   subscriber.next('Charlie');
-//   subscriber.complete();
-// });
-
-// names$.subscribe({
-//   next: value => console.log(value),
-//   complete: () => console.log('Completed')
-// });
-
-    // function ourOwnOf(args: string[]): Observable<string> {
-    //   return new Observable<string>(subscriber => {
-    //     for (let i = 0; i < args.length; i++) {
-    //       subscriber.next(args[i]);
-    //     }
-    //     subscriber.complete();
-    //   });
-    // }
+    forkJoin([randomName$, randomNation$, randomFood$]).subscribe(
+      ([nameAjax, nationAjax, foodAjax]) => console.log(`${nameAjax.response.first_name} is from ${nationAjax.response.capital} and likes to eat ${foodAjax.response.dish}.`)
+    );
   }
 
   run($event) {
