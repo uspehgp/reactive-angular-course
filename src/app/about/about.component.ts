928737bfec8d8a039775608ca0a18eff07fe0bf7
Uspehgp <uspehgp@gmail.com>
2022-01-15 17:01:10 +0300

    implementing interval function

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -29,17 +29,18 @@ export class AboutComponent implements OnInit {
 
     console.log('App started');
 
-    const timer$ = new Observable<number>(subscriber => {
-      const timeoutId = setTimeout(() => {
-        console.log('Timeout!');
-        subscriber.next(0);
-        subscriber.complete();
-      }, 2000);
+    const interval$ = new Observable<number>(subscriber => {
+      let counter = 0;
 
-      return () => clearTimeout(timeoutId);
+      const intervalId = setInterval(() => {
+        console.log('Timeout!');
+        subscriber.next(counter++);
+      }, 1000);
+
+      return () => clearInterval(intervalId);
     });
 
-    const subscription = timer$.subscribe({
+    const subscription = interval$.subscribe({
       next: value => console.log(value),
       complete: () => console.log('Completed')
     });
@@ -47,7 +48,7 @@ export class AboutComponent implements OnInit {
     setTimeout(() => {
       subscription.unsubscribe();
       console.log('Unsubscribe');
-    }, 1000);
+    }, 5000);
 
 // const names$ = new Observable<string>(subscriber => {
 //   subscriber.next('Alice');
